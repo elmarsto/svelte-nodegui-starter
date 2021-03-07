a89e235f79bc49f273a715eb30b58b1947468c1a
Elizabeth Marston <elmarsto@microsoft.com>
2021-03-06 23:46:19 -0800

    nix pkg fix

--- default.nix
+++ default.nix
@@ -1,17 +1,24 @@
 with import <nixpkgs> {};
 let
+  unstable = import
+    #(builtins.fetchTarball https://nixos.org/channels/nixos-unstable)
+    #(builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/staging.tar.gz)
+    (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/master.tar.gz)
+    # reuse the current configuration
+    { config = config; };
+
   npmScript = pkgs.writeShellScriptBin "npm" ''
-    ${pkgs.steam-run}/bin/steam-run ${pkgs.nodejs-14_x}/bin/npm $*
+    ${pkgs.steam-run}/bin/steam-run ${pkgs.unstable.nodejs}/bin/npm $*
   '';
 in
 stdenv.mkDerivation rec {
   name = "svelte-nodegui";
   buildInputs = with pkgs; [
     git
-    cmake
-    steam-run
-    npmScript
-    nodejs-14_x
+    unstable.cmake
+    p7zip
+    unstable.steam-run
+    unstable.nodejs-15_x
   ];
 }
 
